0017f4cab817e36e314c8272a6030147b2f955bb
Hung Ho <hungho66@splabs.info>
2023-04-13 14:26:35 +0700

    init

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+docker stack deploy -c deploy-test.yaml myapp
+# docker stack rm test-deploy
+# docker service ls
+# docker service ps myapp_app
+# docker service logs <tên-service>
+# docker node ls
+# docker system info
+# docker swarm init
+# docker service rm $(docker service ls -q)
+# docker network rm $(docker network ls -q)
+# docker swarm leave
+# docker network ls
